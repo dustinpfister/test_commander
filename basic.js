aa5ed9deecbe48a3d2892b5640e4a6827b2a0a75
dustinpfister <dustin.pfister@gmail.com>
2018-07-10 11:15:37 -0400

    first commit

---
+++ basic.js
@@ -0,0 +1,7 @@
+let prog = require('commander'),
+path = require('path'),
+pkg = require(path.join(__dirname, 'package.json'));
+
+prog
+ .version(pkg.version || '0.0.0','-v, --version')
+ .parse(process.argv);
